091788b511aa6fc24ce8acae631102b9ac86c2c0
toadfanszz <toadfanszz@gmail.com>
2025-09-21 08:28:39 +0800

    feat: add icon tag
    Signed-off-by: toadfanszz <toadfanszz@gmail.com>

--- src/ui/sources/tags.ts
+++ src/ui/sources/tags.ts
@@ -8,28 +8,31 @@ import { partition } from "src/ui/utils";
 
 import { dailyNotes, weeklyNotes } from "../stores";
 
-function getNoteTags(note: TFile | null): string[] {
+function getNoteTags(note: TFile | null): { tags: string[]; icons: string[] } {
   if (!note) {
-    return [];
+    return { tags: [], icons: [] };
   }
 
   const { metadataCache } = window.app;
   const frontmatter = metadataCache.getFileCache(note)?.frontmatter;
 
   const tags = [];
+  const icons = [];
 
   if (frontmatter) {
     const frontmatterTags = parseFrontMatterTags(frontmatter) || [];
     tags.push(...frontmatterTags);
+
+    icons.push(...(frontmatter.icon ? [frontmatter.icon] : []));
   }
 
   // strip the '#' at the beginning
-  return tags.map((tag) => tag.substring(1));
+  return { tags: tags.map((tag) => tag.substring(1)), icons };
 }
 
 function getFormattedTagAttributes(note: TFile | null): Record<string, string> {
   const attrs: Record<string, string> = {};
-  const tags = getNoteTags(note);
+  const { tags, icons } = getNoteTags(note);
 
   const [emojiTags, nonEmojiTags] = partition(tags, (tag) =>
     /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/.test(
@@ -42,6 +45,10 @@ function getFormattedTagAttributes(note: TFile | null): Record<string, string> {
   }
   if (emojiTags) {
     attrs["data-emoji-tag"] = emojiTags[0];
+  }
+
+  if (icons[0]) {
+    attrs["data-icon"] = icons[0];
   }
 
   return attrs;
